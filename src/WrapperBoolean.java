5f0e8dab44a4217c8d6b3a05abada1afdc8a6a98
Efrain Hernandez <efrainhg2050@gmail.com>
2023-03-29 20:31:40 -0600

    How to instance a boolean with class wrapper

--- src/WrapperBoolean.java
+++ src/WrapperBoolean.java
@@ -1,5 +1,11 @@
 public class WrapperBoolean {
     public static void main(String[] args) {
+        //Los valores boolean funcionan muy similar al wrapper integer
+        boolean activo =  1 < 2;
+
+        //La manera de generarlo es con valueOf
+        Boolean activoWraaper = Boolean.valueOf("true");
+        Boolean esTrue = Boolean.valueOf(10 == 10);
 
     }
 }
