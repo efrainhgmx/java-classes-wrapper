0cc6838d293f053f65619fd5517f2d48bef1223d
Efrain Hernandez <efrainhg2050@gmail.com>
2023-04-11 21:26:20 -0600

    Format date example

--- src/DateInJava.java
+++ src/DateInJava.java
@@ -10,8 +10,19 @@ public class DateInJava {
 
         System.out.println("fechaActual = " + fechaActual);
 
-        //Geenera un formato para la fecha
-        SimpleDateFormat formato = new SimpleDateFormat("dd/MMMM/yyyy");
+        //Genera un formato para la fecha
+
+        //11/04/2023
+        SimpleDateFormat formato1 = new SimpleDateFormat("dd/MMMM/yyyy");
+        SimpleDateFormat formato2 = new SimpleDateFormat("dd - MMMM - yyyy");
+        SimpleDateFormat formato3 = new SimpleDateFormat("'fecha' dd/MMMM/yy");
+        //Fecha:  11/abril/23 hora:  10:24:18
+        SimpleDateFormat formato4 = new SimpleDateFormat("'Fecha: ' dd/MMMM/yy 'hora: ' hh:mm:ss");
+
+        //martes 11/abril/23 hora:  10:25:13
+        SimpleDateFormat formato = new SimpleDateFormat("EEEE dd/MMMM/yy 'hora: ' hh:mm:ss");
+
+
 
         String formatDate = formato.format(fechaActual);
         System.out.println("fechaFormato = " + formatDate);
