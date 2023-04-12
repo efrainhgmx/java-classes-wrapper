17b15473d8edc1383058113708047409619e29fb
Efrain Hernandez <efrainhg2050@gmail.com>
2023-04-11 21:10:30 -0600

    Starting with date

--- src/DateInJava.java
+++ src/DateInJava.java
@@ -1,5 +1,10 @@
+import java.util.Date;
+
 public class DateInJava {
     public static void main(String[] args) {
+        //Date es el objeto estandar de Java y es para fechas
 
+        //Forma de instanciar
+        Date fechaActual = new Date();
     }
 }
