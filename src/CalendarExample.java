a54bb1caf67c37e48d76fb39ab91d2ba981c78e3
Efrain Hernandez <efrainhg2050@gmail.com>
2023-04-13 21:28:40 -0600

    Date to String

--- src/CalendarExample.java
+++ src/CalendarExample.java
@@ -1,4 +1,7 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class CalendarExample {
     public static void main(String[] args) {
@@ -7,5 +10,15 @@ public class CalendarExample {
         calendario.set(Calendar.YEAR, Calendar.JULY,Calendar.DATE);
 
         System.out.println("calendario.getTime() = " + calendario.getTime());
+
+        //Convertir una fecha a string
+        SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            Date fecha = formatDate.parse("25/07/1995");
+            System.out.println(fecha);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
