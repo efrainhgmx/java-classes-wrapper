6f1b9a9a6591af6666284e8a147182b6893e08fb
Efrain Hernandez <efrainhg2050@gmail.com>
2023-04-14 19:00:43 -0600

    Add comments

--- src/ReadFromCustomProperties.java
+++ src/ReadFromCustomProperties.java
@@ -4,9 +4,13 @@ import java.util.Properties;
 public class ReadFromCustomProperties {
     public static void main(String[] args) {
         try {
+            //Crea un archivo para leer
             FileInputStream file = new FileInputStream("src/properties/config.properties");
+            //Crea unas propiedades para no mutar sobre system
             Properties prop = new Properties(System.getProperties());
 
+
+            //Carga configuraciones
             prop.load(file);
 
             prop.list(System.out);
