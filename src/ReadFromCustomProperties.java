292d28d756015e2dab3be06cbc0ea5a0d3688170
Efrain Hernandez <efrainhg2050@gmail.com>
2023-04-14 18:59:46 -0600

    Custom property file for custom system property

--- src/ReadFromCustomProperties.java
+++ src/ReadFromCustomProperties.java
@@ -1,5 +1,17 @@
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class ReadFromCustomProperties {
     public static void main(String[] args) {
+        try {
+            FileInputStream file = new FileInputStream("src/properties/config.properties");
+            Properties prop = new Properties(System.getProperties());
 
+            prop.load(file);
+
+            prop.list(System.out);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getCause());
+        }
     }
 }
