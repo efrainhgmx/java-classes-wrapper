3b687f8302f026d3c2ff1ad3ecb3fffb66d7ee8a
Efrain Hernandez <efrainhg2050@gmail.com>
2023-04-28 14:00:42 -0600

    How to exect a program in our SO

---
+++ src/EjecutarProgramaSO.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+public class EjecutarProgramaSO {
+    public static void main(String[] args) {
+
+        //Nos permite ejecutar aplicaciones del sistema operativo
+        Runtime rt = Runtime.getRuntime();
+
+        Process process;
+        //Evalua el sistema operativo
+        try {
+            if(System.getProperty("os.name").startsWith("Windows")) {
+                process = rt.exec("notepad");
+            } else {
+                process =  rt.exec("gedit");
+            }
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            System.err.println("Comando desconocido a ejecutar: " + e.getMessage());
+            System.exit(1);
+        }
+        System.out.println("Cerramos el editor");
+    }
+}
