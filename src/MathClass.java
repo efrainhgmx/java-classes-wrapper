5d4a37cbde723cfc8a3b9d35b03f723853638db8
Efrain Hernandez <efrainhg2050@gmail.com>
2023-04-28 14:19:24 -0600

    Example using class Math

---
+++ src/MathClass.java
@@ -0,0 +1,17 @@
+public class MathClass {
+    public static void main(String[] args) {
+        //Calcular valor absoluto e un numero
+        int absoluto  = Math.abs(-3); // 3
+
+        //Máximo entre 2 numeros
+        double max  = Math.max(3.5, 1.2); // 3.5
+
+        //mínimo entre 2 números
+        double min  = Math.max(3.5, 1.2); //1.2
+
+        double roundUp = Math.ceil(2.86);
+        double roundDown = Math.floor(2.1);
+
+        double round = Math.round(4.75);
+    }
+}
