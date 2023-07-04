c3821cae31a4a96ecfc3924af92c743d97f48941
Efrain Hernandez <efrainhg2050@gmail.com>
2023-07-03 19:00:17 -0600

    Add numbers in bidemensional array

--- src/Matrices.java
+++ src/Matrices.java
@@ -2,5 +2,12 @@ public class Matrices {
     public static void main(String[] args) {
         int [][] numeros = new int[2][4];
         System.out.println("numero de columnas de la matriz = " + numeros.length);
+
+        numeros[0][0] = 1;
+        numeros[0][1] = 2;
+        numeros[0][2] = 3;
+        numeros[0][3] = 4;
+
+        System.out.println("numeros[0][2] = " + numeros[0][2]);
     }
 }
