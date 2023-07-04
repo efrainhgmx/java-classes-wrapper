f99318e40f97e62c1b20db5d7cb61679f37031cd
Efrain Hernandez <efrainhg2050@gmail.com>
2023-07-03 18:56:41 -0600

    Matrices

---
+++ src/Matrices.java
@@ -0,0 +1,6 @@
+public class Matrices {
+    public static void main(String[] args) {
+        int [][] numeros = new int[2][4];
+        System.out.println("numero de columnas de la matriz = " + numeros.length);
+    }
+}
