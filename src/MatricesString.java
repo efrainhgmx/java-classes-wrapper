e835192636281b9f990a559197371adf2c144503
Efrain Hernandez <efrainhg2050@gmail.com>
2023-07-13 18:36:18 -0600

    Another way to defined a matrix

--- src/MatricesString.java
+++ src/MatricesString.java
@@ -18,5 +18,17 @@ public class MatricesString {
             }
         }
 
+        String[][] names = {
+                {"Juan", "Perez"},
+                {"Maria", "Martinez"},
+                {"Pedro", "Gomez"}
+        };
+
+        for(int i = 0; i < names.length; i++){
+            for(int j = 0; j < names[i].length; j++){
+                System.out.println(names[i][j]);
+            }
+        }
+
     }
 }
