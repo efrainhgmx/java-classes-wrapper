b4839bb1c8d8e08ebddf54c644c7939f36f4e2fe
Efrain Hernandez <efrainhg2050@gmail.com>
2023-07-13 18:31:42 -0600

    Print names for a example

--- src/MatricesString.java
+++ src/MatricesString.java
@@ -1,5 +1,22 @@
 public class MatricesString {
     public static void main(String[] args) {
+        //3 rows and 2 columns
         String[][] nombres = new String[3][2];
+
+        nombres[0][0] = "Juan";
+        nombres[0][1] = "Perez";
+
+        nombres[1][0] = "Maria";
+        nombres[1][1] = "Martinez";
+
+        nombres[2][0] = "Pedro";
+        nombres[2][1] = "Gomez";
+
+        for(int i = 0; i < nombres.length; i++){
+            for(int j = 0; j < nombres[i].length; j++){
+                System.out.println(nombres[i][j]);
+            }
+        }
+
     }
 }
