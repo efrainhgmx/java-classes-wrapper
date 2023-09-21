25f16cf5fbd6b7614a620a9c959cf9e92f126d10
Efrain Hernandez <efrainhg2050@gmail.com>
2023-09-21 17:57:29 -0600

    Using better comments

--- src/Automovil.java
+++ src/Automovil.java
@@ -1,5 +1,5 @@
 public class Automovil {
-    //Propiedades o atributos de la clase Automovil
+    //* Propiedades o atributos de la clase Automovil
     String marca;
     String fabricante;
     String modelo;
