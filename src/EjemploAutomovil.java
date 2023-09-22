896b3e228460368c3fba5a93d77527004001db79
Efrain Hernandez <efrainhg2050@gmail.com>
2023-09-21 18:06:21 -0600

    Set a new value for Automovil

--- src/EjemploAutomovil.java
+++ src/EjemploAutomovil.java
@@ -1,6 +1,8 @@
 public class EjemploAutomovil {
     public static void main(String[] args) {
         Automovil auto = new Automovil();
-
+        System.out.println("auto.fabricante = " + auto.fabricante);
+        auto.fabricante = "Nissan";
+        System.out.println("auto.fabricante = " + auto.fabricante);
     }
 }
